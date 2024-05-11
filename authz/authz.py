f7fd5259eb56402ab474f79a274126af5dba1cdb
mostafamohammadi94 <mostafasandman@gmail.com>
2024-05-12 01:54:54 +0330

    config added

--- authz/authz.py
+++ authz/authz.py
@@ -1,6 +1,9 @@
 from flask import Flask 
+from authz.config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    print(app.config)
     return app
 
