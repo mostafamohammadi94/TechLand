7ba95598b20d887f9c26ff36a62090eecf8af063
mostafamohammadi94 <mostafasandman@gmail.com>
2024-07-20 18:22:27 +0330

    bugs fixed

--- authz/model/user.py
+++ authz/model/user.py
@@ -1,30 +1,30 @@
 from uuid import uuid4
-from authz.authz import db
 from authz.config import Config
 from authz.util import now, user_expires_at
+from authz.authz import db
 
 class User(db.Model):
 
-    id = db.Column(db.String(64), primary_key=True, defualt=lambda : uuid4().hex)
+    id = db.Column(db.String(64), primary_key=True, default=lambda : uuid4().hex)
 
     username = db.Column(db.String(128), unique=True, index=True, nullable=False)
 
     password = db.Column(db.String(256), nullable=False)
 
-    role = db.Column(db.String(32), nullable=False, defualt=Config.USER_DEFAULT_ROLE)
+    role = db.Column(db.String(32), nullable=False, default=Config.USER_DEFAULT_ROLE)
 
-    created_at = db.Column(db.DateTime, nullable=False, defualt=now)
+    created_at = db.Column(db.DateTime, nullable=False, default=now)
 
-    expires_at = db.Column(db.DateTime, nullable=False, defualt=user_expires_at)
+    expires_at = db.Column(db.DateTime, nullable=False, default=user_expires_at)
 
-    last_login_at = db.Column(db.DateTime, nullable=True, defualt=None)
+    last_login_at = db.Column(db.DateTime, nullable=True, default=None)
 
-    last_active_at = db.Column(db.DateTime, nullable=True, defualt=None)
+    last_active_at = db.Column(db.DateTime, nullable=True, default=None)
 
-    last_change_at = db.Column(db.DateTime, nullable=True, defualt=None)    
+    last_change_at = db.Column(db.DateTime, nullable=True, default=None)    
 
-    failed_auth_at = db.Column(db.DateTime, nullable=True, defualt=None)
+    failed_auth_at = db.Column(db.DateTime, nullable=True, default=None)
 
-    failed_auth_count = db.Column(db.Intger, Nullable=False, defulat=0)
+    failed_auth_count = db.Column(db.Integer, nullable=False, default=0)
 
     status = db.Column(db.Integer, nullable=False, default=Config.USER_DEFAULT_STATUS)
