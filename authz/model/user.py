99376648b9c8f82de00ab0ef0cfb7a2351ac5b65
mostafamohammadi94 <mostafasandman@gmail.com>
2024-07-20 18:00:52 +0330

    Time added and model updated

--- authz/model/user.py
+++ authz/model/user.py
@@ -1,5 +1,7 @@
 from uuid import uuid4
 from authz.authz import db
+from authz.config import Config
+from authz.util import now, user_expires_at
 
 class User(db.Model):
 
@@ -9,11 +11,11 @@ class User(db.Model):
 
     password = db.Column(db.String(256), nullable=False)
 
-    role = db.Column(db.String(32), nullable=False, defualt=)
+    role = db.Column(db.String(32), nullable=False, defualt=Config.USER_DEFAULT_ROLE)
 
-    created_at = db.Column(db.DateTime, nullable=False, defualt=)
+    created_at = db.Column(db.DateTime, nullable=False, defualt=now)
 
-    expires_at = db.Column(db.DateTime, nullable=False, defualt=)
+    expires_at = db.Column(db.DateTime, nullable=False, defualt=user_expires_at)
 
     last_login_at = db.Column(db.DateTime, nullable=True, defualt=None)
 
@@ -25,4 +27,4 @@ class User(db.Model):
 
     failed_auth_count = db.Column(db.Intger, Nullable=False, defulat=0)
 
-    status = db.Column(db.Integer, nullable=False, default=)
+    status = db.Column(db.Integer, nullable=False, default=Config.USER_DEFAULT_STATUS)
--- authz/util/__init__.py
+++ authz/util/__init__.py
@@ -1,1 +1,4 @@
 from authz.util.jsonify import jsonify
+from authz.util.expires import user_expires_at
+from authz.util import now
+
