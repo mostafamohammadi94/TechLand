f8f293a1543f42d6cc65b1baf02c798ddfef134f
mostafamohammadi94 <mostafasandman@gmail.com>
2024-07-21 16:34:06 +0330

    get_users got OK!

--- authz/controller/apiv1/user.py
+++ authz/controller/apiv1/user.py
@@ -1,10 +1,15 @@
 from authz.model import User
+from authz.schema.apiv1 import UserSchema
 from authz.util import jsonify
 
 class UserController:
 
     def get_users():
-        return jsonify(status=501, code=107)
+        users = User.query.all()
+        users_schema = UserSchema(many=True) 
+        return {
+            "users": users_schema.dump(users)
+        }
 
     def get_user(user_id):
         return jsonify(status=501, code=107)
--- authz/model/user.py
+++ authz/model/user.py
@@ -5,6 +5,8 @@ from authz.util import now, user_expires_at
 
 class User(db.Model):
 
+    __tablename__ = "USERS"
+
     id = db.Column(db.String(64), primary_key=True, default=lambda : uuid4().hex)
 
     username = db.Column(db.String(128), unique=True, index=True, nullable=False)
@@ -28,3 +30,6 @@ class User(db.Model):
     failed_auth_count = db.Column(db.Integer, nullable=False, default=0)
 
     status = db.Column(db.Integer, nullable=False, default=Config.USER_DEFAULT_STATUS)
+
+    def __repr__(self):
+        return f"<Task id='{self.id}' name='{self.name}'>"
