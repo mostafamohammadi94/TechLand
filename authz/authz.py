1f3a720ed186ad70ad560392f5fee5673852a63e
mostafamohammadi94 <mostafasandman@gmail.com>
2024-07-21 15:40:38 +0330

    Schema added!

--- authz/authz.py
+++ authz/authz.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint 
 from flask_restful import Api
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -8,6 +9,8 @@ from authz.config import Config
 
 db = SQLAlchemy()
 mg = Migrate()
+ma = Marshmallow()
+
 
 apiv1_bp = Blueprint("apiv1_bp", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -19,8 +22,10 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     mg.init_app(app, db)
+    ma.init_app(app)
     app.register_blueprint(apiv1_bp)
     return app
+
 
 
 
